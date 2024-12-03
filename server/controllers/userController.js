bb571b684dddc82eba33e196f97be3ac965ba19b
周雨欣 <zhouyuxin@zhouyuxindeMacBook-Air.local>
2024-12-02 21:06:36 -0800

    change auth token in controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -86,15 +86,15 @@ exports.login = async (req, res) => {
             return res.status(401).json({ message: 'Invalid credentials.' });
         }
         const option = {
-            expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-            httpoOnly: true,
+            expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),            // Optional: Expires in 7 days
+            httpOnly: true,
             secure: true,
             sameSite: "none",
             path: "/",
         }
         // If match, create a token
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION });
-        res.status(200).json({ token, user: { userName: user.userName, profilePicture: user.profilePicture } });
+        res.status(200).cookie("authToken", token, option).json({ token, user: { userName: user.userName, profilePicture: user.profilePicture } });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
